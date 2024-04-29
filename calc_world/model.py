b76d1ec53e7d9663851d36bed4e8cbc08f092f98
CONDUCTOR77747 <ammosov.yar@gmail.com>
2024-04-29 17:30:54 +0300

    regular calculator prototyped

--- calc_world/model.py
+++ calc_world/model.py
@@ -4,4 +4,7 @@ Created on Mon Apr 29 16:01:11 2024
 
 @author: conductor
 """
+from django import forms
 
+class CalculatorForm(forms.Form):
+    expression = forms.CharField(label='Expression', max_length=100)
--- calc_world/views.py
+++ calc_world/views.py
@@ -5,6 +5,20 @@ Created on Mon Apr 29 15:17:08 2024
 @author: conductor
 """
 from django.shortcuts import render
+from .model import CalculatorForm
 
 def index(request):
     return render(request, "index.html")
+
+def calculator(request):
+    form = CalculatorForm()
+    result = None
+    if request.method == 'POST':
+        form = CalculatorForm(request.POST)
+        if form.is_valid():
+            expression = form.cleaned_data['expression']
+            try:
+                result = float(eval(expression))
+            except Exception as e:
+                result = 'Error: ' + str(e)
+    return render(request, 'calculator.html', {'form': form, 'result': result})
